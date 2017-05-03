cf5e61d3063e453fe9232dae6f26df4d5ea9e181
Niko <nikolas.hoeft@yahoo.de>
2017-05-03 20:33:45 +0200

    finished markdown file

--- Ex_4_niko.R
+++ Ex_4_niko.R
@@ -21,7 +21,7 @@ get_patch <- function(matrix, h, w, return_vector = FALSE){
         w_patch <- w_sample:(w_sample + w)
     }
     
-    if(return_vector == TRUE){
+    if(return_vector == FALSE){
         return(matrix[h_patch, w_patch])
     }else{
         return(as.vector(matrix[h_patch, w_patch]))
@@ -68,7 +68,7 @@ for(i in 1:length(d)){
     #set.seed(123)
     tmp1 <- t(replicate(500, get_patch(d[[i]], 16, 16, return_vector = TRUE), simplify = "vector"))
     #tmp2 <- replicate(10, get_patch(d[[i]], 16, 16, return_vector = TRUE))
-    total_matrix_b <- rbind(total_matrix, tmp1)
+    total_matrix_b <- rbind(total_matrix_b, tmp1)
 }
 
 
@@ -88,7 +88,7 @@ for(i in 1:length(nat)){
     #set.seed(123)
     tmp1 <- t(replicate(500, get_patch(nat[[i]], 16, 16, return_vector = TRUE), simplify = "vector"))
     #tmp2 <- replicate(10, get_patch(nat[[i]], 16, 16, return_vector = TRUE))
-    total_matrix_n <- rbind(total_matrix, tmp1)
+    total_matrix_n <- rbind(total_matrix_n, tmp1)
 }
 
 
@@ -96,23 +96,86 @@ for(i in 1:length(nat)){
 
 # b)
 
+total_matrix_n_centered <- scale(total_matrix_n, center = TRUE, scale = FALSE)
+total_matrix_b_centered <- scale(total_matrix_b, center = TRUE, scale = FALSE)
+
+pcs_b <- eigen(cov(total_matrix_b_centered))$vectors
+pcs_n <- eigen(cov(total_matrix_n_centered))$vectors
 
 
+amount_pca <- 24
+pca_patchesn <- pca_patchesb <- vector("list", length = amount_pca)
+
+for(i in 1:24){
+    
+    pca_patchesn[[i]] <- melt(matrix(pcs_n[, i], 16, 16))
+    pca_patchesb[[i]] <- melt(matrix(pcs_b[, i], 16, 16))
+    
+}
 
 
+heatmap_custom <- function(matrix){
+    g1 <- ggplot(data = matrix, aes(x=Var1, y=Var2, fill=value)) + 
+        geom_tile() + 
+        scale_fill_continuous(low = "white", high = "black") + 
+        guides(fill = FALSE) +
+        theme(axis.title.x=element_blank(),
+              axis.text.x=element_blank(),
+              axis.ticks.x=element_blank(),
+              axis.title.y=element_blank(),
+              axis.text.y=element_blank(),
+              axis.ticks.y=element_blank())
+                                   
+    
+    return(g1)
+}
 
+plotlist_b <- lapply(pca_patchesb, heatmap_custom)
+plotlist_n <- lapply(pca_patchesn, heatmap_custom)
+
+do.call("grid.arrange", c(plotlist_b, ncol=6))
+do.call("grid.arrange", c(plotlist_n, ncol=6))
+
+
+# One can see that the PCs of both image groups are rather similar, in terms of
+# the variance they account for (i.e. about 65% by the first PC in both cases).
+# However, the second PC of the nature buildings accounts for about 10.7% of the
+# variance while the second PC from the nature images only accounts for about
+# 5.32%
 
 
 
 # c)
 
-pcs_b <- prcomp(total_matrix_b)
-eigenvalues_b <- pcs_b$sdev^2
+#pcs_b <- prcomp(total_matrix_b)
+eigenvalues_b <- eigen(cov(total_matrix_b_centered))$values
 plot(1:256, eigenvalues_b)
 
 cov_b <- cov(total_matrix_b)
 
 
-pcs_n <- prcomp(total_matrix_n)
-eigenvalues_n <- pcs_n$sdev^2
+#pcs_n <- prcomp(total_matrix_n)
+eigenvalues_n <- eigen(cov(total_matrix_n_centered))$values
 plot(1:256, eigenvalues_n)
+
+
+
+
+# d)
+
+no_pcs_ <- c(1, 2, 4, 8, 16, 100)
+
+# buildings
+
+# randomly select three images
+set.seed(12345)
+buildings_sample <- d[sample(1:10, 3)]
+
+y <- buildings_sample[[1]][1:256, 1] %*% pcs_b[,1:256] %*% t(pcs_b[,1:256])
+yy <- buildings_sample[[1]][2:257, 1] %*% pcs_b[,1] %*% t(pcs_b[,1])
+
+
+ggplot(data = melt(matrix(buildings_sample[[1]][1:256, 1])), aes(x=Var1, y=Var2, fill=value)) + 
+    geom_tile() + 
+    scale_fill_continuous(low = "white", high = "black")
+
