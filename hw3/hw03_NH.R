8ff492734a6a2d7a3d5c0af18f4f1466886e94b2
Niko <nikolas.hoeft@yahoo.de>
2017-05-11 21:15:54 +0200

    Ex. 1 and 2 finished

--- hw3/hw03_NH.R
+++ hw3/hw03_NH.R
@@ -3,7 +3,7 @@
 
 library(ggplot2)
 library(dplyr)
-
+library(reshape2)
 
 # 3.1)
 
@@ -49,8 +49,92 @@ par(mfrow = c(1,1))
 
 # 3.2)
 
+# a)
 pca4 <- read.csv("hw3/data/pca4.csv", sep = ",", header = TRUE)
 
 # visual detection of outliers
 pairs(pca4)
 
+par(mfrow = c(2,2))
+boxplot(pca4$X1, main = "V1")
+boxplot(pca4$X2, main = "V2")
+boxplot(pca4$X3, main = "V3")
+boxplot(pca4$X4, main = "V4")
+
+par(mfrow = c(1,1))
+
+# Especially feature 3 and 4 have strong outliers
+
+
+# b)
+
+# center the data 
+pca4_centered <- scale(pca4, center = TRUE, scale = FALSE)
+
+# compute cov matrix
+
+cov_pca4 <- cov(pca4_centered)
+
+pca4_pcs <- eigen(cov_pca4)$vectors
+
+pca4_eigenvals <- eigen(cov_pca4)$values
+
+
+# scree plot
+
+plot(pca4_eigenvals)
+
+# clearly, the fourth PC can be ommitted since it does not provide any 
+# additional information to the first three PCs
+
+
+# c)
+
+# "Whiten" the data, i.e. use Mahalanobis transformation for zero mean and unit sd
+
+Z = pca4_centered %*% pca4_pcs %*% diag((pca4_eigenvals)^-0.5)
+
+# check the transformation:
+
+# means
+round(colMeans(Z), digits = 8)
+
+# standard deviations
+apply(Z, 2, var)
+
+
+# d)
+
+# 1st heatplot: covariance matrix
+
+heatmap_data1 <- melt(cov(pca4_centered))
+
+# plot heatmap 1
+ggplot(data = heatmap_data1, aes(x=Var1, y=Var2, fill=value)) + 
+    geom_tile() + 
+    scale_fill_continuous(low = "white", high = "#b30000") + 
+    ggtitle("Covariance matrix of 'pca4' data")
+
+
+# 2nd heatplot: covariance matrix of data projected onto PC1,...,PC4
+
+projections <- pca4_centered %*% pca4_pcs
+
+heatmap_data2 <- melt(cov(projections))
+
+# plot heatmap 2
+ggplot(data = heatmap_data2, aes(x=Var1, y=Var2, fill=value)) + 
+    geom_tile() + 
+    scale_fill_continuous(low = "white", high = "#b30000") + 
+    ggtitle("Covariance matrix of projections")
+
+
+# 3rd heatplot: covariance matrix of whitened variables
+
+heatmap_data3 <- melt(cov(Z))
+
+# plot heatmap 3
+ggplot(data = heatmap_data3, aes(x=Var1, y=Var2, fill=value)) + 
+    geom_tile() + 
+    scale_fill_continuous(low = "white", high = "#b30000") + 
+    ggtitle("Covariance matrix of whitened data")
