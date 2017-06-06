9b839032ff01c5a96ecaae78f1908b65985f1e33
Niko <nikolas.hoeft@yahoo.de>
2017-06-06 13:03:01 +0200

    neue version

--- hw5/hw5_NH.R
+++ hw5/hw5_NH.R
@@ -4,16 +4,14 @@ library(audio)
 library(ggplot2)
 
 
-#setwd("/Users/maxand22/Google Drive/Humboldt/4. Semester/Machine Intelligence II/Machine_Intelligence_II")
-
-    # Ex. 1 -------------------------------------------------------------------
+# Ex. 1 -------------------------------------------------------------------
 
 # a)
 
 s1 = read.table("hw5/sound1.dat", header=FALSE)
 s2 = read.table("hw5/sound2.dat", header=FALSE)
 
-S = as.matrix(data.frame(s1,s2))
+S = t(as.matrix(data.frame(s1,s2)))
 
 plot(1:18000, s1$V1, type = 'l')
 plot(1:18000, s2$V1, type = 'l')
@@ -27,36 +25,39 @@ play(audioSample(t(as.matrix(s2)), rate = 8192))
 
 # b)
 
-set.seed(123)
-A = matrix(runif(4, 0, 2), nrow = 2)
+set.seed(1234)
+A = matrix(runif(4, 0, 1), nrow = 2)
 
-X = S%*%A
-qplot(1:nrow(X), X[,2])
+X = A%*%S
 
-play(audioSample(t(as.matrix(X[,1])), rate = 8192))
-play(audioSample(t(as.matrix(X[,2])), rate = 8192))
+play(audioSample(t(as.matrix(X[1,])), rate = 8192))
+play(audioSample(t(as.matrix(X[2,])), rate = 8192))
 
 # c)
 
-#X = X[sample(nrow(X)), ]
+X_perm = X[,sample(ncol(X))]
 
-play(audioSample(t(as.matrix(X[,1])), rate = 8192))
-play(audioSample(t(as.matrix(X[,2])), rate = 8192))
+play(audioSample(t(as.matrix(X_perm[1,])), rate = 8192))
+play(audioSample(t(as.matrix(X_perm[2,])), rate = 8192))
 
 # d)
 
-cor(S, X)
-
+cor(S[1,], X[1,])
+cor(S[1,], X[2,])
+cor(S[2,], X[1,])
+cor(S[2,], X[2,])
 
 # e)
 
-X = scale(X, center = TRUE, scale = FALSE)
+X[1,] = X[1,] - mean(X[1,])
+X[2,] = X[2,] - mean(X[2,])
 
-round(colMeans(X), 8)
+
+round(rowMeans(X), 8)
 
 # f)
 
-set.seed(2106)
+set.seed(123)
 W = matrix(runif(4, 0, 1), ncol = 2)
 
 
@@ -70,50 +71,206 @@ f = function(x){
     return(1/(1+exp(-x)))
 }
 
-fdd_over_fd = function(x){
-    return(1 - 2*f(x))
-}
+ggplot(data.frame(x = c(-5, 5)), aes(x)) + stat_function(fun = f)
 
 
 # a)
 
 # for random initialization of weights --> see 1.f)
 t = 1
-eta_zero = 0.1
+eta_zero = .5
+alpha = 1
+set.seed(123)
+W = matrix(runif(4, 0, 1), ncol = 2)
+
+unmixing_regular = function(W, X, n_steps = 18000){
+    for(t in 1:n_steps){
+        eta_t = eta_zero/t
+        
+        x = X[,alpha]
+        
+        W_inv = t(solve(W))
+        
+        f_wx = 1 - 2*f(W%*%cbind(x,x))
+        
+        W_delta = eta_t*(W_inv + f_wx*t(cbind(x,x)))
+        
+        W = W + W_delta
+        
+        alpha = alpha + 1
+        if(alpha == n_steps){
+            alpha = 1
+        }
+    }
+    return(W)
+}
+
+W_regular = unmixing_regular(W, X)
+
+
+S_hat = W_regular%*%X
+play(audioSample(t(as.matrix(scale(S_hat[1,]))), rate = 8192))
+play(audioSample(t(as.matrix(scale(s1))), rate = 8192))
+
+play(audioSample(t(as.matrix(scale(S_hat[2,]))), rate = 8192))
+play(audioSample(t(as.matrix(scale(s2))), rate = 8192))
+
+# b)
+
+t = 1
+eta_zero = 1
 alpha = 1
 set.seed(2106)
 W = matrix(runif(4, 0, 1), ncol = 2)
 
-for(t in 1:18000){
-    eta_t = eta_zero/t
-    
-    x = X[alpha,]
-    
-    w_delta <- eta_t*(t(solve(W)) + (1 - 2*f(W%*%cbind(x, x)))*t(cbind(x,x)))
-    
-    W = W + eta_t*(t(solve(W)) + (1 - 2*f(W%*%cbind(x, x)))*t(cbind(x,x)))
-    
-    alpha = alpha + 1
-    if(alpha == 18000){
-        alpha = 1
+unmixing_natural = function(W, X, n_steps = 18000){
+    for(t in 1:n_steps){
+        eta_t = eta_zero/t
+        
+        x = X[,alpha]
+        
+        W_inv = solve(W)
+        
+        f_wx = 1 - 2*f(W%*%cbind(x,x))
+        
+        W_delta = eta_t*((W_inv + f_wx*rbind(x,x))%*%t(W)%*%W)
+        
+        W = W + W_delta
+        
+        alpha = alpha + 1
+        if(alpha == n_steps){
+            alpha = 1
+        }
     }
+    return(W)
 }
 
-#vvv = X%*%solve(A)
-vvv = X%*%W
-play(audioSample(t(as.matrix(vvv[,1])), rate = 8192))
-play(audioSample(t(as.matrix(vvv[,2])), rate = 8192))
+#W_natural = unmixing_natural(W, X)
+
+#S_hat_natural = W_natural%*%X
+#play(audioSample(t(as.matrix(S_hat_natural[1,])), rate = 8192))
+#play(audioSample(t(as.matrix(S_hat_natural[2,])), rate = 8192))
 
 
 
+# Ex. 3 -------------------------------------------------------------------
 
-W
-solve(A)
 
-qplot(1:nrow(vvv), vvv[,1])
+# a)
 
+# original sounds
+
+plot(1:18000, s1$V1, type = 'l')
+plot(1:18000, s2$V1, type = 'l')
+play(audioSample(t(as.matrix(s1)), rate = 8192))
+play(audioSample(t(as.matrix(s2)), rate = 8192))
+
+# mixed
+
+plot(1:18000, X[1,], type = 'l')
+plot(1:18000, X[2,], type = 'l')
+play(audioSample(t(as.matrix(X[1,])), rate = 8192))
+play(audioSample(t(as.matrix(X[2,])), rate = 8192))
+
+# permuted
+
+plot(1:18000, X_perm[1,], type = 'l')
+plot(1:18000, X_perm[2,], type = 'l')
+play(audioSample(t(as.matrix(X_perm[1,])), rate = 8192))
+play(audioSample(t(as.matrix(X_perm[2,])), rate = 8192))
+
+# recovered
 
 
 
 # b)
+
+
+
+
+# c)
+
+convergence_speed_regular = function(W, X, n_steps = 18000){
+    c_rate = c()
+    for(t in 1:n_steps){
+        eta_t = eta_zero/t
+        
+        x = X[,alpha]
+        
+        W_inv = solve(W)
+        
+        f_wx = 1 - 2*f(W%*%cbind(x,x))
+        
+        W_delta = eta_t*(W_inv + f_wx*rbind(x,x))
+        
+        W = W + W_delta
+        
+        if(t %% 1000 == 0){
+            c_rate = c(c_rate, sum(W^2))
+        }
+        
+        alpha = alpha + 1
+        if(alpha == n_steps){
+            alpha = 1
+        }
+    }
+    return(c_rate)
+}
+
+
+
+
+convergence_speed_natural = function(W, X, n_steps = 18000){
+    c_rate = c()
+    for(t in 1:n_steps){
+        eta_t = eta_zero/t
+        
+        x = X[,alpha]
+        
+        W_inv = solve(W)
+        
+        f_wx = 1 - 2*f(W%*%cbind(x,x))
+        
+        W_delta = eta_t*((W_inv + f_wx*rbind(x,x))%*%t(W)%*%W)
+        
+        W = W + W_delta
+        
+        if(t %% 1000 == 0){
+            c_rate = c(c_rate, sum(W^2))
+        }
+        
+        alpha = alpha + 1
+        if(alpha == n_steps){
+            alpha = 1
+        }
+    }
+    return(c_rate)
+}
+
+
+evec = eigen(cov(t(X)))$vectors
+evals = eigen(cov(t(X)))$values
+
+X_whitened_t = t(X) %*% evec %*% diag((evals)^-0.5)
+round(cor(X_whitened_t), 5)
+
+X_whitened = t(X_whitened_t)
+
+conv_regular = convergence_speed_regular(W, X)
+conv_natural = convergence_speed_natural(W, X)
+conv_regular_whitened = convergence_speed_regular(W, X_whitened)
+conv_natural_whitened = convergence_speed_natural(W, X_whitened)
+    
+# d)
+
+#mixed
+plot(density(X[1,]))
+lines(density(X[2,]))
+
+# unmixed
+
+
+# true signals
+plot(density(s1$V1))
+lines(density(s2$V1))
 
